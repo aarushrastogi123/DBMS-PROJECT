320d1b43cd8dfec82acf82d72b3c1f179c5df7dd
aarushrastogi123 <aarushrastogi123@users.noreply.github.com>
2025-04-22 10:28:47 +0530

    DBMS

---
+++ project.session.sql
@@ -0,0 +1,11 @@
+CREATE TABLE getstarted (
+    started_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    phone VARCHAR(10),
+    role VARCHAR(20),
+    gender VARCHAR(20),
+    email VARCHAR(100)
+);
+
